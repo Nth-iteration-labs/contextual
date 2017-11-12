0333528a92f6518421b442f92ee0c03401757a06
Robin van Emden <robin@pwy.nl>
2017-11-12 17:29:27 +0100

    Second version start

--- R/contextual_offline_evaluation.R
+++ R/contextual_offline_evaluation.R
@@ -9,66 +9,10 @@ OfflineEvaluation <- R6Class(
 
   public = list(
 
-    arms = NULL,
-    num_sims = NULL,
-    horizon = NULL,
-    chosen_arms = NULL,
-    rewards = NULL,
-    sim_nums = NULL,
-    times = NULL,
-    features = NULL,
-    n_arms = NULL,
-    feature_vector = NULL,
-    feature_vector_n = NULL,
-    policy_vector = NULL,
-
     initialize = function(policy = NA, context_arms_matrix = NA, num_sims = NA, horizon = NA, feature_vector = NA) {
 
-      self$num_sims <- num_sims
-      self$horizon <- horizon
-      self$feature_vector <- feature_vector
-      self$feature_vector_n <- length(feature_vector)
-      self$arms <- context_arms_matrix
-      self$n_arms <- length(self$arms)/self$feature_vector_n
-
-      self$policy_vector <- c()
-      for (i in 1:feature_vector_n) self$policy_vector[[i]] <- policy$clone()
-
-      self$chosen_arms        <- rep(0.0, times = num_sims * horizon)
-      self$rewards            <- rep(0.0, times = num_sims * horizon)
-      self$sim_nums           <- rep(0.0, times = num_sims * horizon)
-      self$times              <- rep(0.0, times = num_sims * horizon)
-      self$features           <- rep(0.0, times = num_sims * horizon)
     },
     run = function() {
-      index <- 1L
-      sim <- 1L
-      t <- 1L
-      reward <- 1L
-      chosen_arm <- 1L
-      feature_nr <- 1L
-      # partly replace by apply like functions?
-      for (sim in 1L:self$num_sims) {
-        for (i in 1:feature_vector_n) self$policy_vector[[i]]$reset(self$n_arms)
-        for (t in 1L:self$horizon) {
-          index = (sim - 1L) * self$horizon + t
-
-          # choose which context
-          feature_nr = sample(1:feature_vector_n,1)
-          self$features[index] <- feature_vector[feature_nr]
-
-          self$sim_nums[index] <- sim
-          self$times[index] <- t
-          chosen_arm <- self$policy_vector[[feature_nr]]$select_arm()
-          self$chosen_arms[index] <- chosen_arm
-
-          reward <- self$arms[[feature_nr,chosen_arm]]$draw()
-          self$rewards[index] <- reward
-          self$policy_vector[[feature_nr]]$update(chosen_arm, reward)
-        }
-      }
-      df <- data.frame(self$sim_nums, self$times, self$chosen_arms, self$rewards, self$features)
-      return(df)
     }
   )
 )
--- scripts/demo.R
+++ scripts/demo.R
@@ -1,65 +1,8 @@
 
 # library(contextual)
 
-# setwd("~/GitHub/contextual/scripts")
+setwd("~/GitHub/contextual/scripts")
 
 # if ("package:contextual" %in% search()) detach("package:contextual", unload = TRUE)
 
 source("../R/contextual_utility.R")
-source("../R/contextual_arm_bernoulli.R")
-source("../R/contextual_offline_evaluation.R")
-source("../R/contextual_policy_epsilon_greedy.R")
-
-###################################### basic ######################################
-
-set.seed(10)
-
-# Define a policy
-
-policy                    <- EpsilonGreedy$new(0.1) # multiple polcies should be possible
-
-# Define some contextual features, and their arms.. if one feature, MAB - multiple features: cMAB
-# fun to make possible function that changes over time
-
-pvalues_feature_man       <- c(0.1, 0.1, 0.1, 0.1, 0.9)
-pvalues_feature_woman     <- c(0.1, 0.9, 0.1, 0.1, 0.1)
-pvalues_feature_child     <- c(0.5, 0.1, 0.1, 0.1, 0.1)
-pvalues_feature_cat       <- c(0.1, 0.1, 0.1, 0.3, 0.9)
-pvalues_feature_dog       <- c(0.1, 0.9, 0.2, 0.1, 0.1)
-pvalues_feature_mouse     <- c(0.5, 0.1, 0.1, 0.4, 0.1)
-
-# build a nice matrix out of that
-
-context_pvalues_matrix    <- rbind(pvalues_feature_man,pvalues_feature_woman,pvalues_feature_child,
-                                   pvalues_feature_cat,pvalues_feature_dog,pvalues_feature_mouse)
-context_features_vector   <- rownames(context_pvalues_matrix)
-
-# create context x arms matrix and context feature vector
-
-context_arms_matrix       <- as.matrix(apply(context_pvalues_matrix, c(1,2), BernoulliArm$new))
-
-# Run OfflineEvaluation
-
-num_sims                  <- 300L
-horizon                   <- 250L
-contextual_offline        <- OfflineEvaluation$new(policy, context_arms_matrix, num_sims, horizon, context_features_vector)
-results                   <- contextual_offline$run()
-
-# Print the results
-
-names(results)            <- c("Sim","T","ChosenArm","Reward", "Feature")
-
-# print(results)
-
-###################################### chart ######################################
-
-# for calculation nice to know which optimal
-# optimal <- c(5,2,1) # can also calculate, of course :D cumulative etc.
-
-# chart per feature, all arms, in red best arm.
-
-source("charts.R")
-
-
-
-
