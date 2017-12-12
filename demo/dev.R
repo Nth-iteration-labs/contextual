4f073e14df67da5ec51603ceeb73d404ede921fd
Robin van Emden <robin@pwy.nl>
2017-12-12 14:28:24 +0100

    Remove some more tokens now public repo

--- demo/dev.R
+++ demo/dev.R
@@ -3,8 +3,6 @@ gc()
 if ("package:contextual" %in% search()) detach("package:contextual", unload = TRUE)
 devtools::load_all()
 
-# library(covr); codecov(token = "880ade00-4754-4659-9181-042bb749a94f")
-
 # badge_devel("contextual", "blue")
 
 # theta are all the variables in the policy (function) that we are looking to optimize
