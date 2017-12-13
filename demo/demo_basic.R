8662b80c6bbea4d89c9021b2018e337197cf1ca5
Robin van Emden <robin@pwy.nl>
2017-12-13 16:59:06 +0100

    Minor update basic simulation demo

--- demo/demo_basic.R
+++ demo/demo_basic.R
@@ -6,15 +6,13 @@ source("dev.R")
 
 ############################# basic simulation #################################
 
-bandit         <- SyntheticBandit$new(k = 3L, d = 1L)
+bandit      <- SyntheticBandit$new(k = 3L, d = 1L)$set_weights(c(0.1, 0.1, 0.9))
 
-bandit$set_weights(c(0.1, 0.1, 0.9))
+policy      <- EpsilonGreedyPolicy$new(0.1)
 
-policy         <- EpsilonGreedyPolicy$new(0.1)
-
-agent          <- Agent$new(policy, bandit)
-simulation     <- SimulatorBasic$new(agent)
-history        <- simulation$run(horizon = 100L, simulations = 100L)
+agent       <- Agent$new(policy, bandit)
+simulation  <- SimulatorBasic$new(agent)
+history     <- simulation$run(horizon = 100L, simulations = 100L)
 
 Plot$new()$plot_grid(history)
 
