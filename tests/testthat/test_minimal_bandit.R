6a398a7181fcf714def86ea49392e7d7e1f9ab4a
Robin van Emden <robin@pwy.nl>
2018-01-04 22:54:35 +0100

    Full Bandit test coverage

--- tests/testthat/test_minimal_bandit.R
+++ tests/testthat/test_minimal_bandit.R
@@ -6,9 +6,11 @@ test_that("Minimal bandit setup test", {
   expect_identical(typeof(bandit), "environment")
 
   bandit$set_weights(matrix(c(0.1,0.9,0.1,0.9),2,2))
+
   expect_equal(bandit$k, 2)
   expect_equal(bandit$d, 2)
   expect_false(bandit$is_precaching)
+  expect_warning(bandit$is_precaching <- TRUE,".*locked*")
   expect_identical(bandit$get_weights(), matrix(c(0.1,0.9,0.1,0.9),2,2))
 
   bandit$set_weights(c(0.1,0.9))
@@ -19,8 +21,6 @@ test_that("Minimal bandit setup test", {
   policy      <- EpsilonGreedyPolicy$new()
   expect_identical(typeof(policy), "environment")
 
-
-
   agent       <- Agent$new(policy, bandit)
   expect_identical(typeof(agent), "environment")
 
@@ -30,7 +30,6 @@ test_that("Minimal bandit setup test", {
   expect_identical(bandit$object_size(), bandit$hash)
 
   context <- bandit$get_context()
-
   expect_equal(context$k, 2)
   expect_equal(context$d, 1)
   expect_equal(context$X, 1)
