4b4c8e018d2b9cc1739bccde79d10b444c3c1d50
Robin van Emden <robin@pwy.nl>
2018-01-05 12:33:14 +0100

    Add comments

--- demo/dev.R
+++ demo/dev.R
@@ -1,5 +1,4 @@
-
-## Clean and reset local development environment -------------------------------
+# Clean and reset local development environment --------------------------------
 
 # Detach and unload package, we want to use local code base.
 if ("package:contextual" %in% search()) {
@@ -19,27 +18,26 @@ devtools::load_all()
 # Set environment path to GhostScript command, for CRAN check
 Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.22\\bin\\gswin64c.exe")
 
-## TODO LIST -------------------------------------------------------------------
+
+
+# TODO LIST --------------------------------------------------------------------
 
 # TODO: document, write, document, then write some more
 # TODO: propensity, and correct names for all vars
 # TODO: visualisation(s) from thesis
 # TODO: MODEL type input
 # make sure enough checks on inputs..
-
 # PROGRESS - through sockets?
-
-# check if not precalc faster on 128 cores.
-
+# check if without precalc faster on 128 cores.
+# TODO: seperate context and theta results tables afterall? mem use for nothing.
 # TODO: confidence interval
 # summary -- these agents, ..
 # met continue ding minder duidelijk
 # regret ... tool van analyse...
-
 # TODO: fix external plot device on linux etc
 # TODO: add tests, full test coverage with testthat
 # TODO: theta, cleanup naming
-# TODO: output some info how far along, particularly if not chart --> progressbar
+# TODO: output some info how far along, particularly if not chart -> progressbar
 # TODO: time calculation, benchmarking per policy, how?
 # TODO: double check make sure no object sees/accesses too much
 # TODO: UCB1, exp3, exp4, epoch.. ?? Which other policies, particularly cMAB
@@ -47,14 +45,15 @@ Sys.setenv(R_GSCMD = "C:\\Program Files\\gs\\gs9.22\\bin\\gswin64c.exe")
 # TODO: Ability to add/remove arms
 # TODO: Changes over time
 # TODO: Adversarial
-
 # TODO: blog about all this, do JS versions
 
 
 
+# PERFORMANCE DATA  ------------------------------------------------------------
 
+# on 58  cores:    k3*d3 * 5 policies * 300  * 10000 --> 132 seconds
+# on 120 cores:    k3*d3 * 5 policies * 300  * 10000 --> 390 seconds
 
-
-
-
+# on 58  cores:    k3*d3 * 5 policies * 3000 * 10000 --> 930 seconds
+# on 120 cores:    k3*d3 * 5 policies * 3000 * 10000 --> 691 seconds
 
