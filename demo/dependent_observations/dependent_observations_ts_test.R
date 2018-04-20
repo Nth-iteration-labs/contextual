ccb17691119951a8c141e220319ed40cece798cd
Robin van Emden <robin@pwy.nl>
2018-04-20 13:49:13 +0200

    Dependent observations stan version 0.1

---
+++ demo/dependent_observations/dependent_observations_ts_test.R
@@ -0,0 +1,33 @@
+##################### Imports ###################
+
+#library(contextual)
+library("rstan")
+setwd("~/GitHub/contextual/demo/dependent_observations")
+source("../dev.R")
+source("./bandit_bernoulli.R")
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+##################### Settings ##################
+
+horizon     <- 300
+simulations <- 10
+n_subjects  <- 100
+
+bandit      <- BernoulliBandit$new( n_subjects = 50, arm_one_shape = c(1.5, 1.5), arm_two_shape = c(1.5, 1.5) )
+
+##################### Thompson ###################
+
+# import UnpooledThompson and PooledThompson policies
+source("./policy_pooled_thompson.R")
+
+agents      <- list(Agent$new(PartiallyPooledThompsonPolicy$new(n_subjects = n_subjects, name = "PartialT"), bandit),
+                    Agent$new(PooledThompsonPolicy$new(name = "PooledT"), bandit),
+                    Agent$new(UnpooledThompsonPolicy$new(n_subjects = n_subjects, name = "UnpooledT"), bandit))
+
+history     <- Simulator$new(agents = agents,
+                             horizon = horizon,
+                             simulations = simulations,
+                             do_parallel = FALSE)$run()
+
+plot(history, type = "cumulative", rate = FALSE)
--- demo/dependent_observations/policy_pooled_thompson.R
+++ demo/dependent_observations/policy_pooled_thompson.R
@@ -75,3 +75,151 @@ PooledThompsonPolicy <- R6::R6Class(
     }
   )
 )
+PartiallyPooledThompsonPolicy <- R6::R6Class(
+  "PartiallyPooledThompsonPolicy",
+  portable = FALSE,
+  class = FALSE,
+  inherit = Policy,
+  public = list(
+    n_subjects = NULL,
+    theta_a = NULL,
+    kappa_a = NULL,
+    phi_a = NULL,
+    theta_b = NULL,
+    kappa_b = NULL,
+    phi_b = NULL,
+    theta_samples_a = NULL,
+    phi_samples_a = NULL,       # why? deleted.
+    kappa_samples_a = NULL,     # why? deleted.
+    theta_samples_b = NULL,
+    phi_samples_b = NULL,       # why? deleted.
+    kappa_samples_b = NULL,     # why? deleted.
+    fit_a = NULL,
+    fit_b = NULL,
+    initialize = function(n_subjects = 1, name = "PartiallyPooledThompsonPolicy") {
+      super$initialize(name)
+      self$n_subjects = n_subjects
+    },
+    set_parameters = function() {
+      self$theta <- list(n = list(rep(0,self$n_subjects),rep(0,self$n_subjects)),  # TODO: make this into k-arms 0, not 0,0
+                         l = list(rep(0,self$n_subjects),rep(0,self$n_subjects)))  # TODO: make this into k-arms 0, not 0,0
+      self$theta_to_arms <- list('n_zero' = 0)
+      ## theta, phi, kappa (for computing posterior)
+      ## For this we just run Stan once so that we can use the parameters in a loop later
+      n_subjects <- self$n_subjects
+      n <- theta$n[[1]]
+      l <- theta$l[[1]]
+      self$fit_a <-
+        stan(
+          "beta_binom_hier_a.stan",
+          data = c("n_subjects", "n", "l"),
+          iter = 20,
+          warmup = 10,
+          chains = 1,
+          seed = 1234  ### TODO: use main seed here!
+        )
+      self$theta_a <- summary(self$fit_a, pars = c("theta"))$summary[, "mean"]
+      self$kappa_a <- summary(self$fit_a, pars = c("kappa"))$summary[, "mean"]
+      self$phi_a <- summary(self$fit_a, pars = c("phi"))$summary[, "mean"]
+      n_subjects <- self$n_subjects
+      n <- theta$n[[2]]
+      l <- theta$l[[2]]
+      self$fit_b <-
+        stan(
+          "beta_binom_hier_b.stan",
+          data = c("n_subjects", "n", "l"),
+          iter = 20,
+          warmup = 10,
+          chains = 1,
+          seed = 1234  ### TODO: use main seed here!
+        )
+      self$theta_b <- summary(self$fit_b, pars = c("theta"))$summary[, "mean"]
+      self$kappa_b <- summary(self$fit_b, pars = c("kappa"))$summary[, "mean"]
+      self$phi_b <- summary(self$fit_b, pars = c("phi"))$summary[, "mean"]
+
+      # Furthermore, we need to keep a list theta_samples
+
+      self$theta_samples_a <- extract(self$fit_a, pars = c("theta"))$theta
+      self$theta_samples_b <- extract(self$fit_b, pars = c("theta"))$theta
+    },
+    get_action = function(context, t) {
+      userid <- context$user_context
+      if (self$theta_samples_a[theta$n[[1]][userid] %% 10 + 1, userid] > self$theta_samples_b[theta$n[[2]][userid] %% 10 + 1, userid]) {
+        action$choice = 1
+      } else {
+        action$choice = 2
+      }
+      action
+    },
+    set_reward = function(context, action, reward, t) {
+      arm      <- action$choice
+      userid   <- context$user_context
+      reward   <- reward$reward
+
+      # First update user parameters
+      if (arm == 1) {
+        theta$n[[1]][userid] <- theta$n[[1]][userid] + 1
+        theta$l[[1]][userid] <- theta$l[[1]][userid] + reward
+        theta$n_zero[[1]] <- theta$n_zero[[1]] + 1
+        if (theta$n_zero[[1]] %% 10 == 0) {
+          init_val_a <-
+            list(list(
+              theta = self$theta_a,
+              kappa = self$kappa_a,
+              phi = self$phi_a
+            ))
+          n_subjects <- self$n_subjects
+          n <- theta$n[[1]]
+          l <- theta$l[[1]]
+          self$fit_a <-
+            stan(
+              "beta_binom_hier_a.stan",
+              data = c("n_subjects", "n", "l"),
+              iter = 20,
+              warmup = 10,
+              init = init_val_a,
+              chains = 1,
+              seed = 1234
+            )
+          self$theta_a <- summary(self$fit_a, pars = c("theta"))$summary[, "mean"]
+          self$kappa_a <- summary(self$fit_a, pars = c("kappa"))$summary[, "mean"]
+          self$phi_a <- summary(self$fit_a, pars = c("phi"))$summary[, "mean"]
+          self$theta_samples_a <- extract(self$fit_a, pars = c("theta"))$theta
+        }
+      } else {
+        # arm b
+        theta$n[[2]][userid] <- theta$n[[2]][userid] + 1
+        theta$l[[2]][userid] <- theta$l[[2]][userid] + reward
+        theta$n_zero[[2]] <- theta$n_zero[[2]] + 1
+        if (theta$n_zero[[2]] %% 10 == 0) {
+          init_val_b <-
+            list(list(
+              theta = self$theta_b,
+              kappa = self$kappa_b,
+              phi = self$phi_b
+            ))
+          n_subjects <- self$n_subjects
+          n <- theta$n[[2]]
+          l <- theta$l[[2]]
+          self$fit_b <-
+            stan(
+              "beta_binom_hier_b.stan",
+              data = c("n_subjects", "n", "l"),
+              iter = 20,
+              warmup = 10,
+              init = init_val_b,
+              chains = 1,
+              seed = 1234
+            )
+          self$theta_b <- summary(self$fit_b, pars = c("theta"))$summary[, "mean"]
+          self$kappa_b <- summary(self$fit_b, pars = c("kappa"))$summary[, "mean"]
+          self$phi_b <- summary(self$fit_b, pars = c("phi"))$summary[, "mean"]
+          self$theta_samples_b <- extract(self$fit_b, pars = c("theta"))$theta
+          self$phi_samples_b <- extract(self$fit_b, pars = c("phi"))$phi
+          self$kappa_samples_b <- extract(self$fit_b, pars = c("kappa"))$kappa
+        }
+      }
+      theta
+    }
+  )
+)
