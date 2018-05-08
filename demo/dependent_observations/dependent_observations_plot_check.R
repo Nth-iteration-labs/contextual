f8e9a25d9eef137850f400d3face51f7d9656a14
Robin van Emden <robin@pwy.nl>
2018-05-08 11:20:11 +0200

    Dependent observations demo updates

---
+++ demo/dependent_observations/dependent_observations_plot_check.R
@@ -0,0 +1,20 @@
+### Imports
+
+setwd("~/GitHub/contextual/demo/dependent_observations")
+source("../dev.R")
+
+data_dir        <- "D:/data/"
+history         <- History$new()
+history$load_data(paste0(data_dir,"UnpooledEG_b1.5_s50_p0_r100.RData"))
+cum_regret <- history$cumulative(final = TRUE, rate = FALSE, regret = TRUE)
+print(cum_regret)
+
+history$clear_data_table()
+history$load_data(paste0(data_dir,"PooledEG_b1.5_s50_p0_r100.RData"))
+cum_regret <- history$cumulative(final = TRUE, rate = FALSE, regret = TRUE)
+print(cum_regret)
+
+history$clear_data_table()
+history$load_data(paste0(data_dir,"PartialEG_b1.5_s50_p0_r100.RData"))
+cum_regret <- history$cumulative(final = TRUE, rate = FALSE, regret = TRUE)
+print(cum_regret)
