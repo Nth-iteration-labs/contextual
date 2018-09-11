028ac24700e0cd9358a7e708fe4489b5b3c3388f
robinvanemden <robin@pwy.nl>
2018-09-11 17:58:01 +0200

    Doc update

--- tests/testthat/test_agent.R
+++ tests/testthat/test_agent.R
@@ -21,6 +21,7 @@ test_that("Agent", {
   history            <- Simulator$new(agents = agent,
                                       horizon = 10,
                                       simulations = 10,
+                                      do_parallel = FALSE,
                                       progress_file = TRUE)$run()
 
   expect_identical(history$cumulative$testme$reward,0.3)
--- tests/testthat/test_policy.R
+++ tests/testthat/test_policy.R
@@ -1,12 +1,17 @@
 context("Policy")
 
-test_that("Policy check", {
+test_that("Policy", {
 
-  policy <- Policy$new()
+  policy      <- Policy$new()
   expect_identical(typeof(policy), "environment")
 
-  expect_error(policy$get_action(1,2), ".*implement.")
-  expect_error(policy$set_reward(1,2), ".*implement.")
-  expect_error(policy$set_parameters(), ".*implement.")
+  policy$theta_to_arms <- list(n=3)
+  theta <- policy$initialize_theta(4)
+  expect_identical(theta$n[[4]], 3)
+
+  expect_identical(policy$class_name, "Policy")
+  expect_error(policy$get_action(), "has not been implemented")
+  expect_error(policy$set_parameters(), "has not been implemented")
+  expect_error(policy$set_reward(), "has not been implemented")
 
 })
