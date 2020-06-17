a3ac8c4336a3b6090859338b339c646b0f518f9c
Leandro Ferrado <ljferrado@gmail.com>
2020-06-16 22:59:56 -0300

    Added test for Exp3Policy

--- tests/testthat/test_policies.R
+++ tests/testthat/test_policies.R
@@ -18,3 +18,22 @@ test_that("ContextualLogitBTSPolicy simulation", {
   expect_equal(history$cumulative$ContextualLogitBTS$cum_regret,  11.6, tolerance = 0.01)
 
 })
+
+test_that("Exp3Policy simulation", {
+
+  horizon       <- 20L
+  simulations   <- 20L
+  weights       <- c(0.9, 0.1, 0.1)
+
+  policy        <- Exp3Policy$new(gamma = 0.1)
+  bandit        <- BasicBernoulliBandit$new(weights = weights)
+  agent         <- Agent$new(policy, bandit)
+
+  simulation    <- Simulator$new(agent, horizon, simulations, do_parallel = FALSE)
+  history       <- simulation$run()
+
+  expect_equal(history$cumulative$Exp3$cum_reward,  7.5,  tolerance = 0.01)
+  expect_equal(history$cumulative$Exp3$cum_regret,  10.8, tolerance = 0.01)
+
+})
+
